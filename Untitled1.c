89994a1a6da0bf3286cdff9e42527ffe5f4f5097
Saanvi Gupta <124417848+SaanviGupta0312@users.noreply.github.com>
2023-03-13 20:25:12 +0530

    initial commit

---
+++ Untitled1.c
@@ -0,0 +1,55 @@
+#include<stdio.h>
+int main()
+/*
+{
+   char a;
+   printf("Enter character: ");
+   scanf("%c",&a);
+   if (a>='A' && a<='Z')
+   {
+       printf("It is a capital alphabet");
+   }
+   else if (a>='a' && a<='z')
+   {
+       printf("Lower case aphabet");
+   }
+   else if (a>='0' && a<='9')
+   {
+       printf("digit");
+   }
+   else
+   {
+       printf("Special character");
+   }
+   return 0;
+}
+*/
+
+/*
+{
+    int unit;
+    printf("Enter units: ");
+    scanf("%d",&unit);
+    if (unit<=50 && unit>=0)
+    {
+        printf("%d",unit*7);
+    }
+    else if (unit>50 && unit<=150)
+    {
+        printf("%d",(unit)*9);
+    }
+    else if (unit>150 && unit<=250)
+    {
+        printf("%d",unit*12);
+    }
+    else if (unit>250)
+    {
+        printf("%d",unit*15);
+    }
+    else
+    {
+        printf("Invalid input");
+    }
+    return 0;
+}
+*/
